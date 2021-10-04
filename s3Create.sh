702c711824e451eabbdd4360e845aac957db2440
threemay <254363807@qq.com>
2021-10-04 21:23:26 +1100

    1

--- s3Create.sh
+++ s3Create.sh
@@ -9,6 +9,6 @@ terraform init
 terraform apply --auto-approve
 
 # upload files to s3
-bucketname=$(terraform output bucket_name)
-aws s3 cp ./index.html s3://${bucketname}
+bucketname=$(terraform output bucket_name | sed 's/"//g')
+aws s3 cp ../public/index.html s3://${bucketname}
 
