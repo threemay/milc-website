fd9d9400fbe9845a53df7e2f066ad64a35b8800e
Haley-Zhu <Haley.ZhuY@gmail.com>
2021-10-04 16:10:57 +1100

    reconfig prettierrc & eslint
    1. reconfig prettierrc & eslint
    2. add editorconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,77 +1,117 @@
+const OFF = 0
+const WARN = 1
+const ERROR = 2
+
 module.exports = {
 	env: {
 		browser: true,
-		es2020: true,
+		es2021: true,
 		node: true,
 	},
-	plugins: ['prettier', '@typescript-eslint'],
-	extends: ['airbnb-typescript', 'react-app', 'prettier'],
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+		'airbnb/hooks',
+		'plugin:unicorn/recommended',
+		'plugin:promise/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		project: './tsconfig.json',
-		tsconfigRootDir: __dirname,
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
 	},
 	settings: {
 		'import/resolver': {
-			typescript: {
-				alwaysTryTypes: true,
+			node: {
+				extensions: ['.tsx', '.ts', '.js', '.json'],
 			},
 		},
 	},
+	plugins: ['react', '@typescript-eslint'],
 	rules: {
-		'object-curly-spacing': ['warn', 'always'],
-		'no-unused-vars': [
-			'warn',
+		'import/extensions': [
+			ERROR,
+			'ignorePackages',
 			{
-				vars: 'all',
-				args: 'none',
+				ts: 'never',
+				tsx: 'never',
+				js: 'never',
 			},
 		],
-		'@typescript-eslint/semi': ['off'],
-		'@typescript-eslint/no-unused-vars': [
-			'warn',
+		'import/no-extraneous-dependencies': [ERROR, { devDependencies: true }],
+		'import/prefer-default-export': OFF,
+		'import/no-unresolved': ERROR,
+		'import/no-dynamic-require': OFF,
+
+		'unicorn/better-regex': ERROR,
+		'unicorn/prevent-abbreviations': OFF,
+		'unicorn/filename-case': [
+			ERROR,
 			{
-				vars: 'all',
-				args: 'none',
+				cases: {
+					// 中划线
+					kebabCase: true,
+					// 小驼峰
+					camelCase: true,
+					// 下划线
+					snakeCase: false,
+					// 大驼峰
+					pascalCase: true,
+				},
 			},
 		],
-		'@typescript-eslint/no-explicit-any': [
-			'error',
+		'unicorn/no-array-instanceof': WARN,
+		'unicorn/no-for-loop': WARN,
+		'unicorn/prefer-add-event-listener': [
+			ERROR,
 			{
-				ignoreRestArgs: true,
+				excludedPackages: ['koa', 'sax'],
 			},
 		],
-		indent: ['error', 'tab', { SwitchCase: 1 }],
-		'max-len': [
-			'warn',
-			{
-				code: 80,
-				ignoreStrings: true,
-				ignoreTemplateLiterals: true,
-				ignoreComments: true,
-			},
-		],
-		'no-plusplus': [
-			'error',
-			{
-				allowForLoopAfterthoughts: true,
-			},
-		],
-		'react/jsx-key': 'error',
-		'import/no-extraneous-dependencies': [
-			'error',
-			{
-				devDependencies: true,
-			},
-		],
-		'react/jsx-props-no-spreading': 'off',
-		'import/prefer-default-export': 'off',
-		'react/jsx-boolean-value': 'off',
-		'react/prop-types': 'off',
-		'react/no-unescaped-entities': 'off',
-		'react/jsx-one-expression-per-line': 'off',
-		'react/jsx-wrap-multilines': 'off',
-		'react/destructuring-assignment': 'off',
-		'@typescript-eslint/no-explicit-any': ['off'],
+		'unicorn/prefer-query-selector': ERROR,
+		'unicorn/no-null': OFF,
+		'unicorn/no-array-reduce': OFF,
+
+		'@typescript-eslint/no-useless-constructor': ERROR,
+		'@typescript-eslint/no-empty-function': WARN,
+		'@typescript-eslint/no-var-requires': OFF,
+		'@typescript-eslint/explicit-function-return-type': OFF,
+		'@typescript-eslint/explicit-module-boundary-types': OFF,
+		'@typescript-eslint/no-explicit-any': OFF,
+		'@typescript-eslint/no-use-before-define': ERROR,
+		'@typescript-eslint/no-unused-vars': WARN,
+		'no-unused-vars': OFF,
+
+		'react/jsx-filename-extension': [ERROR, { extensions: ['.tsx', 'ts', '.jsx', 'js'] }],
+		'react/jsx-indent-props': [ERROR, 2],
+		'react/jsx-indent': [ERROR, 2],
+		'react/jsx-one-expression-per-line': OFF,
+		'react/destructuring-assignment': OFF,
+		'react/state-in-constructor': OFF,
+		'react/jsx-props-no-spreading': OFF,
+		'react/prop-types': OFF,
+
+		'jsx-a11y/click-events-have-key-events': OFF,
+		'jsx-a11y/no-noninteractive-element-interactions': OFF,
+		'jsx-a11y/no-static-element-interactions': OFF,
+
+		'lines-between-class-members': [ERROR, 'always'],
+		// indent: [ERROR, 2, { SwitchCase: 1 }],
+		'linebreak-style': [ERROR, 'unix'],
+		quotes: [ERROR, 'single'],
+		semi: [ERROR, 'always'],
+		'no-unused-expressions': WARN,
+		'no-plusplus': OFF,
+		'no-console': OFF,
+		'class-methods-use-this': ERROR,
+		'jsx-quotes': [ERROR, 'prefer-single'],
+		'global-require': OFF,
+		'no-use-before-define': OFF,
+		'no-restricted-syntax': OFF,
+		'no-continue': OFF,
 	},
 }
